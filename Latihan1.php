cf66e931ca4119c65f2e83279d890dfa19caa522
Azel 14 <19250930@bsi.ac.id>
2026-04-17 15:07:33 +0700

    first commit

---
+++ Latihan1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan</title>
+</head>
+<body>
+    <h1>Latihan1</h1>
+    <table border="1">
+        <tr>
+        <td>Nama</td>
+        <td>Usia</td>
+        <td>No Hp</td>
+        </tr>
+        <tr>
+        <td>Ajel</td>
+        <td>19</td>
+        <td>082125470166</td>
+        </tr>
+    </table>
+</body>
+</html>
